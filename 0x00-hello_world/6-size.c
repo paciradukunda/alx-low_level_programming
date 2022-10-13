96a2dfecacb5515be308992e4f1854964625bccb
paciradukunda <paifique@gmail.com>
2022-10-13 08:34:09 +0000

    bash srcpits that compiles c programs

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("Size of a char: %d byte(s)",sizeof(char));
+	printf("Size of an int: %d byte(s)",sizeof(int));
+	printf("Size of a long int: %d byte(s)",sizeof(long));
+	printf("Size of a long long int: %d byte(s)",sizeof(double));
+	printf("Size of a float: %d byte(s)",sizeof(float));
+	return 0;
+}
