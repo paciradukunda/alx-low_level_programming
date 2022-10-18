be59479b064d411f0045d692841b7f26cf0916f1
IRADUKUNDA PACIFIQUE <99093836+paciradukunda@users.noreply.github.com>
2022-10-18 15:00:01 +0200

    betty correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,12 +3,14 @@
 /**
  * main - prints _putchar
  *
+ * Description: prints _putchar using putchar prototype
+ *
  * return: 0 if succefull
  */
 
 int main(void)
 {
-	int strg[] = {95,112,117,116,99,104,97,114};
+	int strg[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int size, i;
 
 	size = sizeof(strg) / sizeof(int);
