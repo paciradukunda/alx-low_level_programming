751f92461911e1698d3711d90e3fc4e0c0435359
IRADUKUNDA PACIFIQUE <99093836+paciradukunda@users.noreply.github.com>
2022-10-18 15:07:51 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,18 +5,17 @@
  *
  * Description: prints _putchar using putchar prototype
  *
- * return: 0 if succefull
+ * Return: 0 if succefull
  */
 
 int main(void)
 {
-	int strg[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int size, i;
 
-	size = sizeof(strg) / sizeof(int); 
+	char str[] = "_putchar";
+	int ch;
 
-	for (i = 0;i < size;i++)
-		_putchar(strg[i]);
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
 	_putchar('\n');
 
 	return (0);
