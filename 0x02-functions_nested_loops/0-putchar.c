0cce6f1c202f74cc404e7279aac012613365f23b
IRADUKUNDA PACIFIQUE <99093836+paciradukunda@users.noreply.github.com>
2022-10-18 15:03:42 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,7 +13,7 @@ int main(void)
 	int strg[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int size, i;
 
-	size = sizeof(strg) / sizeof(int);
+	size = sizeof(strg) / sizeof(int); 
 
 	for (i = 0;i < size;i++)
 		_putchar(strg[i]);
