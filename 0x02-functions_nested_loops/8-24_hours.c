151c1c525835ceb42b9b31bd4b75f9f9d5ed325d
IRADUKUNDA PACIFIQUE <99093836+paciradukunda@users.noreply.github.com>
2022-10-18 15:32:06 +0200

    error

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,13 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * jack_bauer- prints every minute of the day
- */
+*/
 
 void jack_bauer(void)
 {
 	int min, hr;
-	
+
 	for (hr = 0; hr <= 23; ++hr)
 	{
 		for (min = 0; min <= 59; ++min)
