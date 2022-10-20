3df97ca64d0e02c83879fa2bbe1a2868f0cea9a3
IRADUKUNDA PACIFIQUE <99093836+paciradukunda@users.noreply.github.com>
2022-10-20 16:06:42 +0200

    correcting comment error

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,12 @@
-#include "main.h"
-
 /**
   * _isdigit - checks if charchter is a number
   *
-  * @c - integer input
+  * @c: integer input
   *
   * Return: 1 if digit 0 otherwise
   */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
 	if (c >= 0 && c <= 9)
 		return (1);
