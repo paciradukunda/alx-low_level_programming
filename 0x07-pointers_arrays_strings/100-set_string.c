53a754f2bc13965462122b2bf28f4561fe0bc077
IRADUKUNDA PACIFIQUE <99093836+paciradukunda@users.noreply.github.com>
2022-11-01 19:38:03 +0200

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,9 +2,10 @@
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: source adress.
- * @to: target adress.
- * Return: no return.
+ *
+ * @s: double pointer
+ * @to: pointer
+ * Return: void
  */
 void set_string(char **s, char *to)
 {
