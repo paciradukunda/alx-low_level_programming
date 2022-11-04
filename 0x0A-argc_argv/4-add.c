fe84639a53f83dce843b373cb4503cb18db4a69f
IRADUKUNDA PACIFIQUE <99093836+paciradukunda@users.noreply.github.com>
2022-11-04 20:48:38 +0200

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,26 +1,33 @@
 #include "main.h"
-/**
-  * main - adds two numbers
-  * @argc: size of array
-  * @argv: arry of parameters
-  *
-  * RETURN: 0 if succefull
-  */
 
+/**
+ * main - adds positive numbers
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
 int main(int argc, char *argv[])
 {
-	int sum, i;
+	int i, j, sum;
 
-	if(argc == 1)
+	if (argc == 1)
 	{
-		printf("%d\n", 0);
-		return (1);
-	}
-	else
-	{
-		for(i = 0; i < argc; i++)
-		       if(	
-		product = argv[1] + argv[2];
+		printf("0\n");
 		return (0);
 	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '-'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
