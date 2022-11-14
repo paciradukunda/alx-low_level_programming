0c36a3899f0392228ad0bda41dee2fa3da66456f
paciradukunda <paifique@gmail.com>
2022-11-14 11:43:58 +0000

    editing file 3 and 4

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
 
-#define ABS(X) ((X < 0) ? (X * -1) : (X))
+#define ABS(X) (((X) < 0) ? -(X) : (X))
 
-#endif // _FUNCTION_LIKE_MACRO_H
+#endif /* _FUNCTION_LIKE_MACRO_H */
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -3,4 +3,4 @@
 
 #define SUM(x, y) (x + y)
 
-#endif // _SUM_H
+#endif /* _SUM_H */
