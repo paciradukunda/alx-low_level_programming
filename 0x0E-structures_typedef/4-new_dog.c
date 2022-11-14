4f521fe7f1859442b090ff517eb216c28ae0ef7e
IRADUKUNDA PACIFIQUE <99093836+paciradukunda@users.noreply.github.com>
2022-11-14 19:42:23 +0200

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "dog.h"
 /**
   * new_dog - creates a dog data type with values
   *
@@ -10,14 +11,42 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new;
+	dog_t *p_dog;
+	int i, lname, lowner;
 
-	new = malloc(sizeof(dog_t));
-	if (new == NULL)
+	p_dog = malloc(sizeof(*p_dog));
+	if (p_dog == NULL || !(name) || !(owner))
+	{
+		free(p_dog);
 		return (NULL);
-	new -> name = name;
-	new -> age = age;
-	new -> owner = owner;
+	}
 
-	return (new);
+	for (lname = 0; name[lname]; lname++)
+		;
+
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
+
+	p_dog->name = malloc(lname + 1);
+	p_dog->owner = malloc(lowner + 1);
+
+	if (!(p_dog->name) || !(p_dog->owner))
+	{
+		free(p_dog->owner);
+		free(p_dog->name);
+		free(p_dog);
+		return (NULL);
+	}
+
+	for (i = 0; i < lname; i++)
+		p_dog->name[i] = name[i];
+	p_dog->name[i] = '\0';
+
+	p_dog->age = age;
+
+	for (i = 0; i < lowner; i++)
+		p_dog->owner[i] = owner[i];
+	p_dog->owner[i] = '\0';
+
+	return (p_dog);
 }
