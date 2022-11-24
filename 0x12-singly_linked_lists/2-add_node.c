9d2e00e5be72168a1a776c4d6261c85ddb7f35c2
IRADUKUNDA PACIFIQUE <99093836+paciradukunda@users.noreply.github.com>
2022-11-24 20:40:26 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,24 +1,45 @@
 #include "main.h"
 /**
-  * @brief function adds a new node to the head of linked list. 
-  * 
-  * @head: the head of the linked list.
-  * @str: the data for new node.
-  *
-  * Return: node address
-  */
+ * add_node - adds a node at the beginning of the linked list
+ *
+ * @head: pointer to the first node of the list
+ * @str: string to add to new node in the list
+ *
+ * Return: NULL if it fails / starting addr of the list
+*/
+
 list_t *add_node(list_t **head, const char *str)
 {
-	struct node* tmp = malloc(sizeof(list_t));
-	struct node* new = malloc(sizeof(list_t));
-	char dup = malloc(sizeof
+	list_t *new_node;
+	size_t str_len = 0;
 
-	tmp = *head;
+	/*set strig length to 0 if its NULL*/
+	if (str == NULL)
+		str_len = 0;
 
-        new->data = data;
-	new->link = first;
-	first = new;
+	/*count length of string*/
+	while (str[str_len] != '\0')
+		str_len++;
 
-	return (first);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
+	/**
+	 * initialize the next addr of new_node
+	 * to NULL if head is NULL, if it's not
+	 * move the first node to the addr of new_node
+	 * ->next hence adding new_node at the beginning
+	 *  of the list
+	*/
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+
+	new_node->str = strdup(str);
+	new_node->len = str_len;
+	*head = new_node;
+
+	return (*head);
 }
