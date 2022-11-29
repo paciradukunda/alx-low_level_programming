5e084e604ca4b52c83c66b4b60978861707b56e9
paciradukunda <paifique@gmail.com>
2022-11-29 10:46:55 +0000

    error handling

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,12 +10,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *tmp = malloc(sizeof(listint_t));
 	listint_t *new = malloc(sizeof(listint_t));
-
+	
 	tmp = *head;
 
 	new->n = n;
 	new->next = *head;
 	*head = new;
 
-	return (*head);
+	return (head);
 }
