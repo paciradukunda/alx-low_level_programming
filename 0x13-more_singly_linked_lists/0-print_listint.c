ed77477fbee5487a53fa8d0e21a2f472fb3279ce
IRADUKUNDA PACIFIQUE <99093836+paciradukunda@users.noreply.github.com>
2022-11-29 20:25:55 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,16 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
-
-	if (h == NULL)
-		return (NULL);
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
-		count++;
 		printf("%d\n", h->n);
 		h = h->next;
+		nnodes++;
 	}
-	return (count);
+	return (nnodes);
 }
